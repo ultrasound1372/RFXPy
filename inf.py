7e8a8b2bd7ba35e5d548107b9da94a500463bf4c
Colton Hill <ultrasound2001@gmail.com>
2022-03-11 11:35:06 -0600

    Documented some misc python files.

--- inf.py
+++ inf.py
@@ -1,4 +1,13 @@
-#! /usr/bin/env python3.7
+#! /usr/bin/env python3
+# -*- Coding: UTF-8 -*-
+# infinity test case
+# The port seems to differ from the original code here
+# the original code will madly clip this string
+# while this code seems to ignore the infinity and just produces a normal wave
+# unsure of which behavior is desireable at this time
+# or how to get this to behave like the original
+# that version of PureBasic appears to ignore division by 0 and give inf
+# but sign is undetermined, I don't have that version to test with
 import RFX
 g=RFX.SFXRWave()
 g.load('AQAAAM3MTL0AAAAAAAAAABSuxz4K1yO+zcwMP/Yo3D4fhWs+KVyPPQAAgD6PwnU+AAAAAI/C9TwAAEC/mplZP8P1KL/NzEw9j8J1Pj0K1757FC4+zczMPs3MTL3Xo3A/DwA=')
--- testrfx.py
+++ testrfx.py
@@ -1,5 +1,9 @@
 #! /usr/bin/env python3
 # -*- coding: UTF-8 -*-
+# CLI tester for RFX synth
+# used during synth development
+# $ python -i testrfx.py
+
 import RFX
 import wx
 import wx.adv
@@ -9,27 +13,32 @@ App=wx.App()
 s=wx.adv.Sound()
 stack=deque(maxlen=5)
 def p():
+    """Plays the sound at the top of the stack, or nothing if stack is empty."""
     try:
         s.CreateFromData(stack[-1].Create())
         s.Play()
     except IndexError:
         pass
 def r():
+    """Adds a randomized sound to the stack."""
     stack.append(RFX.SFXRWave.randomize())
     p()
 def m():
+    """Adds a mutation of the last item to the stack."""
     try:
         stack.append(stack[-1].mutate())
         p()
     except IndexError:
         print("Nothing to mutate.")
 def undo():
+    """Undoes the last operation, returns to previous item on stack."""
     try:
         stack.pop()
         p()
     except IndexError:
         print("Nothing to undo.")
 def l(s):
+    """Loads a sound from the string given as the single argument."""
     try:
         g=RFX.SFXRWave()
         g.load(s)
@@ -38,6 +47,8 @@ def l(s):
     except ValueError:
         print("Bad data")
 def w(name):
+    """Writes the current item to a wave file given by the single parameter.
+    Parameter must not have extension."""
     try:
         f=open(name+'.wav','wb')
         f.write(stack[-1].Create())
@@ -45,4 +56,5 @@ def w(name):
     except IndexError:
         print("Nothing to write")
 def g():
+    """Just gets the last item on the stack and returns it. Useful for changing synth params."""
     return stack[-1]
