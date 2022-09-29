ff5ced370d15c5442ab06d380de671a47c5f9b6a
meatbag93 <cccefg2@gmail.com>
2022-09-29 23:35:19 +0300

    Start implementing the GUI.

--- inf.py
+++ inf.py
@@ -10,7 +10,7 @@
 # but sign is undetermined, I don't have that version to test with
 import RFX
 g=RFX.SFXRWave()
-g.load('AQAAAM3MTL0AAAAAAAAAABSuxz4K1yO+zcwMP/Yo3D4fhWs+KVyPPQAAgD6PwnU+AAAAAI/C9TwAAEC/mplZP8P1KL/NzEw9j8J1Pj0K1757FC4+zczMPs3MTL3Xo3A/DwA=')
+g.load('AQAAAM3MTL0AAAAAAAAAABSuxz4K1yO+zcwMP/Yo3D4fhWs+KVyPPQAAgD6PwnU+AAAAAI/C9TwAAEC/mplZP8P1KL/NzEw9j8J1Pj0K1757FC4+zczMPs3MTL3Xo3A/DwS=')
 f=open('inf.wav','wb')
 f.write(g.Create())
 f.close()
